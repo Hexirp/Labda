8dcfacba23c63143687005d0e74ee2f0fcb62961
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2023-08-15 03:28:34 +0900

    Add: TermLabdaPure を
    副作用については、少し実装が厄介になりそうなので、まずは純粋な
    サブセットから実装を行なう。
    
    命名においては、基本的に「それが何であるか」を表す一つの単語を
    先頭に置き、その後に詳細な情報を装飾する単語を並べる方法で行なう。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+enum TermLabdaPure {
+    Var { name: String },
+    App {
+        term_left: Box<TermLabdaPure>,
+        term_right: Box<TermLabdaPure>,
+        strategy: bool,
+    },
+    Lam { name: String, term: Box<TermLabdaPure> },
+    Dup {
+        name_copied: String,
+        name_copy_1: String,
+        name_copy_2: String,
+        term: Box<TermLabdaPure>,
+    },
+    Des { name_dropped: String, term: Box<TermLabdaPure> },
+}
+
 fn main() {
     println!("Hello, world!");
 }
