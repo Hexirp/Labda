777fcd78d711cc16141794701f8c52e3d6f81867
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2023-08-17 21:14:45 +0900

    Add: is_used_free 関数を
    x が t の自由変数であるか判定する関数は基本操作であるため。

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,45 @@ impl TermLabdaPure {
             }
         }
     }
+
+    fn is_used_free(&self, name_target: &String) -> bool {
+        match self {
+            TermLabdaPure::Var { name } => name == name_target,
+            TermLabdaPure::App { term_left, term_right, .. } => {
+                let x = term_left.is_used_free(name_target);
+                let y = term_right.is_used_free(name_target);
+                x || y
+            }
+            TermLabdaPure::Lam { name, term } => {
+                if name == name_target {
+                    false
+                } else {
+                    term.is_used_free(name_target)
+                }
+            }
+            TermLabdaPure::Dup {
+                name_copied,
+                name_copy_1,
+                name_copy_2,
+                term,
+            } => {
+                let x = name_copied == name_target;
+                let y = name_copy_1 == name_target;
+                let z = name_copy_2 == name_target;
+                let w = if y || z {
+                    false
+                } else {
+                    term.is_used_free(name_target)
+                };
+                x || w
+            }
+            TermLabdaPure::Des { name_dropped, term } => {
+                let x = name_dropped == name_target;
+                let y = term.is_used_free(name_target);
+                x || y
+            }
+        }
+    }
 }
 
 fn main() {
