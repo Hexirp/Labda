a826d6ad72ce11b7f2e08d80eb74ab5590337062
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2024-05-23 17:18:39 +0900

    Add: LambdaCalculusExpression を定義する
    ラムダ計算の項を表すために。
    
    LambdaCalculusVariableName も定義する。
    
    String を使うのはラムダ計算の変数の名前を表すためだということが明確
    になるように。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Clone, Debug)]
+struct LambdaCalculusVariableName { string: String }
+
+#[derive(Clone, Debug)]
+enum LambdaCalculusExpression {
+    Variable { name: LambdaCalculusVariableName },
+    Application { function_part: Box<LambdaCalculusExpression>, argument_part: Box<LambdaCalculusExpression> },
+    LambdaAbstraction { variable_name: LambdaCalculusVariableName, expression: Box<LambdaCalculusExpression> },
+}
+
 fn main() {
     println!("Hello, world!");
+    let x = LambdaCalculusVariableName { string: "a".to_string() };
+    println!("{:?}", x);
+    let y = LambdaCalculusExpression::Variable { name: x.clone() };
+    println!("{:?}", y);
+    let z = LambdaCalculusExpression::Application { function_part: Box::new(y.clone()), argument_part: Box::new(y.clone()) };
+    println!("{:?}", z);
+    let w = LambdaCalculusExpression::LambdaAbstraction { variable_name: x.clone(), expression: Box::new(y.clone()) };
+    println!("{:?}", w);
+    let v = LambdaCalculusExpression::LambdaAbstraction { variable_name: x.clone(), expression: Box::new(z.clone()) };
+    println!("{:?}", v);
 }
