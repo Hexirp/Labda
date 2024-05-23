c90168484243c29b3f9b7090a1a1d032423d449c
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2024-05-23 17:55:39 +0900

    Update: print デバッグの部分を更新する
    変数の名前が足りないので連番に変更する。

--- src/main.rs
+++ src/main.rs
@@ -23,14 +23,16 @@ impl LambdaCalculusVariableName {
 
 fn main() {
     println!("Hello, world!");
-    let x = LambdaCalculusVariableName { string: "a".to_string() };
-    println!("{:?}", x);
-    let y = LambdaCalculusExpression::Variable { name: x.clone() };
-    println!("{:?}", y);
-    let z = LambdaCalculusExpression::Application { function_part: Box::new(y.clone()), argument_part: Box::new(y.clone()) };
-    println!("{:?}", z);
-    let w = LambdaCalculusExpression::LambdaAbstraction { variable_name: x.clone(), expression: Box::new(y.clone()) };
-    println!("{:?}", w);
-    let v = LambdaCalculusExpression::LambdaAbstraction { variable_name: x.clone(), expression: Box::new(z.clone()) };
-    println!("{:?}", v);
+    let x_000 = LambdaCalculusVariableName { string: "a".to_string() };
+    println!("{:?}", x_000);
+    let x_001 = LambdaCalculusExpression::Variable { name: x_000.clone() };
+    println!("{:?}", x_001);
+    let x_002 = LambdaCalculusExpression::Application { function_part: Box::new(x_001.clone()), argument_part: Box::new(x_001.clone()) };
+    println!("{:?}", x_002);
+    let x_003 = LambdaCalculusExpression::LambdaAbstraction { variable_name: x_000.clone(), expression: Box::new(x_001.clone()) };
+    println!("{:?}", x_003);
+    let x_004 = LambdaCalculusExpression::LambdaAbstraction { variable_name: x_000.clone(), expression: Box::new(x_002.clone()) };
+    println!("{:?}", x_004);
+    let x_005 = &x_000.is_used_in(&x_001);
+    println!("{:?}", x_005)
 }
