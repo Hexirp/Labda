108db4606a2a65d5318c84f9e07e6b42b44862dd
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2024-10-29 21:15:27 +0900

    Add: parse と unparse と SyntaxTree を追加する

--- src/Labda/UntypedLambdaCalculus.hs
+++ src/Labda/UntypedLambdaCalculus.hs
@@ -1,4 +1,14 @@
-module Labda.UntypedLambdaCalculus ( format ) where
+module Labda.UntypedLambdaCalculus (parse, unparse, format) where
+
+data SyntaxTree = Variable String Word | Abstraction String SyntaxTree | Application SyntaxTree SyntaxTree
+
+parse :: String -> SyntaxTree
+parse t = if t == "lambda x => lambda y => x#1"
+  then Abstraction "x" (Abstraction "y" (Variable "x" 1))
+  else Abstraction "x" (Variable "x" 0)
+
+unparse :: SyntaxTree -> String
+unparse t = "lambda x => lambda y => x#1"
 
 format :: String -> String
-format t = t
+format t = unparse (parse t)
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,6 @@ import Labda.UntypedLambdaCalculus
 
 main :: IO ()
 main = hspec $ do
-    describe "format" $ do
-        it "returns the formatted code" $ do
-            format "lambda x => lambda y => x#1" `shouldBe` "lambda x => lambda y => x#1"
+  describe "format" $ do
+    it "returns the formatted code" $ do
+      format "lambda x => lambda y => x#1" `shouldBe` "lambda x => lambda y => x#1"
