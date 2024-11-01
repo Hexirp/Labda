e8d93d0a0dfe3d9361af84c7dda0df2d805a2fa2
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2024-11-01 21:00:41 +0900

    Update: テストケースを正常化する
    ログの仕様が固まってきたので、このようにする。

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,15 +10,58 @@ main :: IO ()
 main = hspec $ do
   describe "symbol" $ do
     it "is fine" $ do
-      runParser (symbol "lambda") "lambda" `shouldBe` Success [] "" ()
+      runParser (symbol "lambda") "lambda" `shouldBe` Success
+        [ IsMatch 'l' 'l'
+        , IsMatch 'a' 'a'
+        , IsMatch 'm' 'm'
+        , IsMatch 'b' 'b'
+        , IsMatch 'd' 'd'
+        , IsMatch 'a' 'a'
+        ]
+        ""
+        ()
     it "is fine 2" $ do
-      runParser (symbol "lambda") "lain" `shouldBe` Failure []
+      runParser (symbol "lambda") "lain" `shouldBe` Failure
+        [ IsMatch 'l' 'l'
+        , IsMatch 'a' 'a'
+        , IsNotMatch (NoEndChar 'i') 'm'
+        ]
     it "is fine 3" $ do
-      runParser (symbol "lain" <|> symbol "lambda") "lambda" `shouldBe` Success [] "" ()
+      runParser (symbol "lain" <|> symbol "lambda") "lambda" `shouldBe` Success
+        [ IsMatch 'l' 'l'
+        , IsMatch 'a' 'a'
+        , IsNotMatch (NoEndChar 'm') 'i'
+        , IsMatch 'l' 'l'
+        , IsMatch 'a' 'a'
+        , IsMatch 'm' 'm'
+        , IsMatch 'b' 'b'
+        , IsMatch 'd' 'd'
+        , IsMatch 'a' 'a'
+        ]
+        ""
+        ()
     it "is fine 4" $ do
-      runParser (symbol "JavaScript") "Java" `shouldBe` Failure []
+      runParser (symbol "JavaScript") "Java" `shouldBe` Failure
+        [ IsMatch 'J' 'J'
+        , IsMatch 'a' 'a'
+        , IsMatch 'v' 'v'
+        , IsMatch 'a' 'a'
+        , IsNotMatch End 'S'
+        ]
     it "is fine 5" $ do
-      runParser (symbol "JavaScript" <|> symbol "Java") "Java" `shouldBe` Success [] "" ()
+      runParser (symbol "JavaScript" <|> symbol "Java") "Java" `shouldBe` Success
+        [ IsMatch 'J' 'J'
+        , IsMatch 'a' 'a'
+        , IsMatch 'v' 'v'
+        , IsMatch 'a' 'a'
+        , IsNotMatch End 'S'
+        , IsMatch 'J' 'J'
+        , IsMatch 'a' 'a'
+        , IsMatch 'v' 'v'
+        , IsMatch 'a' 'a'
+        ]
+        ""
+        ()
   describe "format" $ do
     it "returns the formatted code" $ do
       format "lambda x => lambda y => x#1" `shouldBe` "lambda x => lambda y => x#1"
