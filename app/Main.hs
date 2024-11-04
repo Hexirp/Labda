e3259a1d0dc4f45f7c3f1ad03a90aa2f0e839efa
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2024-11-04 21:13:24 +0900

    Update: Main.hs を更新する

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,24 @@ import Control.Applicative
 import Labda.Parser
 import Labda.UntypedLambdaCalculus
 
+printParserResult :: Show a => ParserResult [String] String a -> IO ()
+printParserResult r = case r of
+  Failure w -> do
+    putStrLn "= Failure ="
+    putStrLn ""
+    putStrLn $ unlines w
+  Success w s a -> do
+    putStrLn "= Success ="
+    putStrLn ""
+    putStrLn $ unlines w
+    print s
+    putStrLn ""
+    print a
+    putStrLn ""
+
 main :: IO ()
 main = do
-  print $ runParser (symbol "lain" <|> symbol "lambda") "lambda"
-  print $ runParser (symbol "JavaScript" <|> symbol "Java") "Java"
-  print $ runParser parseTerm "lambda x => lambda y => x#1"
+  printParserResult $ runParser (symbol "lain" <|> symbol "lambda") "lambda"
+  printParserResult $ runParser (symbol "JavaScript" <|> symbol "Java") "Java"
+  printParserResult $ runParser parseTerm "lambda x => lambda y => x#1"
+  printParserResult $ runParser parseTerm "lambda x => ( lambda y => x#1 ( y#0 y#0 ) ) ( lambda y => x#1 ( y#0 y#0 ) )"
